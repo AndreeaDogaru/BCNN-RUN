3e41ecc204ffd6267fe21004ed47a3f2b93ede25
TArdelean <timitoc@gmail.com>
2020-12-14 13:11:41 +0200

    Added improved BCNN

--- source/models.py
+++ source/models.py
@@ -4,6 +4,7 @@
 
 import torch
 import torchvision
+from torch.autograd import Function
 
 torch.manual_seed(41)
 torch.cuda.manual_seed_all(41)
@@ -42,8 +43,76 @@ class BCNN(torch.nn.Module):
         X = X.view(-1, 512 ** 2)
 
         # BCNN Normalization
-        X = torch.sqrt(X + 1e-5)
+        X = X.sign().mul(torch.sqrt(X.abs() + 1e-5))
         X = torch.nn.functional.normalize(X)
+        X = self.fc(X)
+
+        return X
+
+
+# Kudos https://github.com/DennisLeoUTS/improved-bilinear-pooling/blob/master/utils/bilinear_layers.py
+def sqrt_newton_schulz(A, num_iter):
+    batch_size = A.shape[0]
+    dim = A.shape[1]
+    norm_A = A.mul(A).sum(dim=1).sum(dim=1).sqrt()
+    Y = A.div(norm_A.view(batch_size, 1, 1).expand_as(A))
+    I = torch.eye(dim, dim).view(1, dim, dim).repeat(batch_size, 1, 1).type(torch.cuda.FloatTensor)
+    Z = torch.eye(dim, dim).view(1, dim, dim).repeat(batch_size, 1, 1).type(torch.cuda.FloatTensor)
+    for i in range(num_iter):
+        T = 0.5 * (3.0 * I - Z.bmm(Y))
+        Y = Y.bmm(T)
+        Z = T.bmm(Z)
+    return Y * torch.sqrt(norm_A).view(batch_size, 1, 1).expand_as(A)
+
+
+def lyap_newton_schulz(z, dldz, num_iter):
+    batch_size = z.shape[0]
+    dim = z.shape[1]
+    norm_z = z.mul(z).sum(dim=1).sum(dim=1).sqrt()
+    a = z.div(norm_z.view(batch_size, 1, 1).expand_as(z))
+    I = torch.eye(dim, dim).view(1, dim, dim).repeat(batch_size, 1, 1).type(torch.cuda.FloatTensor)
+    q = dldz.div(norm_z.view(batch_size, 1, 1).expand_as(z))
+    for i in range(num_iter):
+        q = 0.5 * (q.bmm(3.0 * I - a.bmm(a)) - a.transpose(1, 2).bmm(a.transpose(1, 2).bmm(q) - q.bmm(a)))
+        a = 0.5 * a.bmm(3.0 * I - a.bmm(a))
+    dlda = 0.5 * q
+    return dlda
+
+
+class matrix_sqrt(Function):
+    @staticmethod
+    def forward(ctx, x):
+        output = sqrt_newton_schulz(x, 10)
+        ctx.save_for_backward(output)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = ctx.saved_tensors[0]
+        grad_input = lyap_newton_schulz(output, grad_output, 10)
+        return grad_input
+
+
+class IBCNN(BCNN):
+
+    def __init__(self, freeze_features):
+        super().__init__(freeze_features)
+        self.matrix_sqrt = matrix_sqrt.apply
+
+    def forward(self, X):
+        X = self.features(X)
+        X = X.view(-1, 512, 28 ** 2)
+        X = torch.bmm(X, torch.transpose(X, 1, 2)) / (28 ** 2)  # Bilinear
+
+        # I-BCNN Normalization
+        X = self.matrix_sqrt(X)
+
+        X = X.view(-1, 512 ** 2)
+
+        # BCNN Normalization
+        X = X.sign().mul(torch.sqrt(X.abs() + 1e-5))
+        X = torch.nn.functional.normalize(X)
+
         X = self.fc(X)
 
         return X
